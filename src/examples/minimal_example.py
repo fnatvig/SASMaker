174c055bba7aa8adaea54f0fd876e5de14874af2
fnatvig <filip@natvig.se>
2025-09-04 17:44:13 +0200

    working version

--- src/examples/minimal_example.py
+++ src/examples/minimal_example.py
@@ -3,9 +3,9 @@ from sasmaker.builder import snap_child_to_slot
 from sasmaker.plotting import plot_one_line
 # from sasmaker.simulation import Simulation, sample_cts, sample_cbs, sample_buses, inject_overcurrent_on_line_to_bus, sample_ieds
 from sasmaker.simulation import Simulation, inject_overcurrent_on_line_to_bus, sample_ieds
-from sasmaker.util import generate_values_df
+from sasmaker.util import generate_values_df, create_interfaces, spawn_script
 
-s = Substation("Larger substation")
+s = Substation("Simple substation")
 
 # Adding busbars
 bExt = s.add_busbar("S/S-230/66kV", vn_kv=66, x=4.0, y=1.0, draw_slots=1, ext_grid=True)
@@ -44,12 +44,14 @@ s.add_load("LoadA", b2, p_mw=14.5, q_mvar=9.5)
 s.add_load("LoadB", b3, p_mw=14.5, q_mvar=9.5)
 
 # Adding IEDs and connecting them to one CT and one CB each
-ied1 = s.add_ied("ied1", ct=ct1, cb=cb1)
+ied1 = s.add_ied("IED1", ct=ct1, cb=cb1)
 ied1.ptoc.pickup_ka = 0.45
-ied2 = s.add_ied("ied2", ct=ct2, cb=cb2)
+ied2 = s.add_ied("IED2", ct=ct2, cb=cb2)
 ied2.ptoc.pickup_ka = 0.2
-ied3 = s.add_ied("ied3", ct=ct3, cb=cb3)
+ied3 = s.add_ied("IED3", ct=ct3, cb=cb3)
 ied3.ptoc.pickup_ka = 0.2
+# ied4 = s.add_ied("IED4", ct=ct3, cb=cb3)
+# ied4.ptoc.pickup_ka = 0.2
 
 
 # Simulation conf
@@ -68,12 +70,22 @@ sim_data = sim.run(s)
 
 # Export CSVs (for ied comm)
 ieds = [ied1, ied2, ied3]
+
+create_interfaces(numIEDs = len(ieds))
+
 for ied in ieds:
     df = generate_values_df(sim_data, ied)
-    df.to_csv(f"output/csv/{ied.name}.csv") 
+    df.to_csv(f"./toolchain/SASMaker_{ied.name}/value.csv", header=False) 
+
+
+
+
+spawn_script(
+        cwd="./toolchain",
+        py_paths=["./toolchain"])
 
 # Export complete simulation
-sim_data.to_csv("output/simulation.csv")
+# sim_data.to_csv("output/simulation.csv")
 
 # plot last state of system
 plot_one_line(s, line_color="#009B24", buslink_color="#555555", buslink_style="-",
--- src/sasmaker/util.py
+++ src/sasmaker/util.py
@@ -1,8 +1,17 @@
 # sasmaker/util.py
+import os
+import atexit
+import signal
+import sys
+import subprocess
 import pandas as pd
 import numpy as np
+from pathlib import Path
+
+
 
 BUS_REF_COLS = {"bus", "from_bus", "to_bus", "hv_bus", "lv_bus"}
+
 
 def _fix_ref_cols(df, name):
     if df is None or df.empty:
@@ -50,8 +59,8 @@ def replace_nan_with_zero(df: pd.DataFrame) -> pd.DataFrame:
 def generate_values_df(df, ied):
     col_headers = ["Circuit breaker Open/Close Status", 
             "Disconnector Open/Close Status", 
-            "Earth switch Open/Close Status", 
-            "Protection system healthy (Trip circuit healthy)", 
+            # "Earth switch Open/Close Status", 
+            # "Protection system healthy (Trip circuit healthy)", 
             "Control level - Local or remote", 
             "Protection tripped", 
             "Circuit breaker mechanical failure", 
@@ -88,36 +97,128 @@ def generate_values_df(df, ied):
     pf = (P.abs() / S).fillna(0.0)
     df_new = pd.DataFrame()
 
+    # df_new[col_headers[0]] = list(cb_status)
+    # df_new[col_headers[1]] = len(df)*[1]
+    # df_new[col_headers[2]] = len(df)*[0]
+    # df_new[col_headers[3]] = len(df)*[1]
+    # df_new[col_headers[4]] = len(df)*["FALSE"]
+    # df_new[col_headers[5]] = ["TRUE" if i else "FALSE" for i in cb_tripped]
+    # df_new[col_headers[6]] = len(df)*[1]
+    # df_new[col_headers[7]] = len(df)*["FALSE"]
+    # df_new[col_headers[8]] = ["TRUE" if i else "FALSE" for i in cb_tripped]
+    # df_new[col_headers[9]] = len(df)*["FALSE"]
+    
+    # # currents
+    # df_new[col_headers[10]] = list(current_a.astype('int32'))
+    # df_new[col_headers[11]] = list(current_b.astype('int32'))
+    # df_new[col_headers[12]] = list(current_c.astype('int32'))
+
+    # # voltages
+    # df_new[col_headers[13]] = list(voltage_a.astype('int32'))
+    # df_new[col_headers[14]] = list(voltage_b.astype('int32'))
+    # df_new[col_headers[15]] = list(voltage_c.astype('int32'))
+
+    # # powers
+    # df_new[col_headers[16]] = list(P.astype('int32'))
+    # df_new[col_headers[17]] = list(Q.astype('int32'))
+    
+    # # frequency
+    # df_new[col_headers[18]] = [50.0 if i!=0 else 0 for i in list(P.astype('int32'))] 
+
+    # # power factor
+    # df_new[col_headers[19]] = list(np.round(pf, decimals=2))
+
     df_new[col_headers[0]] = list(cb_status)
+    # df_new[col_headers[1]] = len(df)*[1]
+    # df_new[col_headers[2]] = len(df)*[0]
     df_new[col_headers[1]] = len(df)*[1]
-    df_new[col_headers[2]] = len(df)*[0]
-    df_new[col_headers[3]] = len(df)*[1]
-    df_new[col_headers[4]] = len(df)*["FALSE"]
-    df_new[col_headers[5]] = ["TRUE" if i else "FALSE" for i in cb_tripped]
-    df_new[col_headers[6]] = len(df)*[1]
+    df_new[col_headers[2]] = len(df)*["FALSE"]
+    df_new[col_headers[3]] = ["TRUE" if i else "FALSE" for i in cb_tripped]
+    df_new[col_headers[4]] = len(df)*[1]
+    df_new[col_headers[5]] = len(df)*["FALSE"]
+    df_new[col_headers[6]] = ["TRUE" if i else "FALSE" for i in cb_tripped]
     df_new[col_headers[7]] = len(df)*["FALSE"]
-    df_new[col_headers[8]] = ["TRUE" if i else "FALSE" for i in cb_tripped]
-    df_new[col_headers[9]] = len(df)*["FALSE"]
     
     # currents
-    df_new[col_headers[10]] = list(current_a.astype('int32'))
-    df_new[col_headers[11]] = list(current_b.astype('int32'))
-    df_new[col_headers[12]] = list(current_c.astype('int32'))
+    df_new[col_headers[8]] = list(current_a.astype('int32'))
+    df_new[col_headers[9]] = list(current_b.astype('int32'))
+    df_new[col_headers[10]] = list(current_c.astype('int32'))
 
     # voltages
-    df_new[col_headers[13]] = list(voltage_a.astype('int32'))
-    df_new[col_headers[14]] = list(voltage_b.astype('int32'))
-    df_new[col_headers[15]] = list(voltage_c.astype('int32'))
+    df_new[col_headers[11]] = list(voltage_a.astype('int32'))
+    df_new[col_headers[12]] = list(voltage_b.astype('int32'))
+    df_new[col_headers[13]] = list(voltage_c.astype('int32'))
 
     # powers
-    df_new[col_headers[16]] = list(P.astype('int32'))
-    df_new[col_headers[17]] = list(Q.astype('int32'))
+    df_new[col_headers[14]] = list(P.astype('int32'))
+    df_new[col_headers[15]] = list(Q.astype('int32'))
     
     # frequency
-    df_new[col_headers[18]] = [50.0 if i!=0 else 0 for i in list(P.astype('int32'))] 
+    df_new[col_headers[16]] = [50.0 if i!=0 else 0 for i in list(P.astype('int32'))] 
 
     # power factor
-    df_new[col_headers[19]] = list(np.round(pf, decimals=2))
+    df_new[col_headers[17]] = list(np.round(pf, decimals=2))
 
     
     return df_new
+
+def _cleanup_interfaces(numIEDs: int):
+    # Delete macvlan children first
+    for i in range(1, numIEDs + 1):
+        name = f"veth1.{i}"
+        os.system(f"sudo ip link set {name} down >/dev/null 2>&1")
+        os.system(f"sudo ip link delete {name} >/dev/null 2>&1")
+    # Then delete the parent
+    os.system("sudo ip link set veth1 down >/dev/null 2>&1")
+    os.system("sudo ip link delete veth1 >/dev/null 2>&1")
+
+def _signal_handler(sig, frame, numIEDs):
+    _cleanup_interfaces(numIEDs)
+    sys.exit(0)
+
+def create_interfaces(numIEDs):
+
+    print("SASMaker v1.0")
+    print("Interfaces Setup")
+
+    # automatic cleanup wiring
+    atexit.register(_cleanup_interfaces, numIEDs)
+    signal.signal(signal.SIGINT,  lambda s, f: _signal_handler(s, f, numIEDs))
+    signal.signal(signal.SIGTERM, lambda s, f: _signal_handler(s, f, numIEDs))
+
+    print("Enabling dummy kernel module")
+    os.system ("sudo modprobe dummy")
+
+    print("Creating a virtual interface")
+    os.system ("sudo ip link add veth1 type dummy")
+
+    print("Giving the virtual interface a MAC address")
+    os.system ("sudo ifconfig veth1 hw ether A2:2E:D6:80:A8:FF")
+
+    print("Giving the interface an alias and IP address")
+    os.system ("sudo ip addr add 192.168.1.100/24 brd + dev veth1 label veth1:0")
+
+    print("put the interface up")
+    os.system ("sudo ip link set dev veth1 up")
+
+    for x in range (numIEDs):
+            ied = x+1
+            print("Creating and enabling the virtual interface for IED"+str(ied))
+            os.system("sudo ip link add link veth1 address 'A2:2E:D6:80:A8:"+str(ied*11)+"' veth1."+str(ied)+" type macvlan mode bridge")
+            os.system("sudo ifconfig veth1."+str(ied)+" up")
+
+def spawn_script(cwd=None, python=None, py_paths=None):
+    script = "toolchain.py"
+    python = python or sys.executable
+    argv = [python, script]
+
+    env = os.environ.copy()
+    if py_paths:
+        env["PYTHONPATH"] = os.pathsep.join(py_paths)
+
+    p = subprocess.Popen(argv, cwd=cwd, env=env)
+    print(f"Spawned {script} (pid={p.pid}) from {cwd or os.getcwd()}")
+    return p
+
+
+
