8e58bc95ffac0e9e59640ba28796610049dd1a90
fnatvig <filip@natvig.se>
2025-11-17 17:18:32 +0100

    minor fix

--- src/examples/biswas_example.py
+++ src/examples/biswas_example.py
@@ -182,11 +182,11 @@ s.add_load("Feeder 6\nPriority 6", b14,   p_mw=4.9, q_mvar=2.8)
 
 
 # --- sim ---
-sim_len = 60
+sim_len = 600
 # sim = Simulation("step load", t_end=sim_len, dt=1.037, vary_loads=True)
 sim = Simulation("step load", t_end=sim_len, dt=0.98, vary_loads=True)
 # inject_overcurrent_on_line_to_bus(sim, t0=5, duration=3, line_name="L3", factor=2)
-# trigger_busbar_protection(sim, ied_name="LIED10", busbar_name="66kV bus-1", t0=9)
+trigger_busbar_protection(sim, ied_name="LIED10", busbar_name="66kV bus-1", t0=9)
 sim.add_sampler(sample_ieds())
 sim_data = sim.run(s)
 
@@ -197,7 +197,7 @@ ieds = [ied1, ied2, ied3, ied4,
         iedx1, iedx2, iedx3, iedx4,
         ied100, ied200] 
 
-# create_interfaces(ieds)
+create_interfaces(ieds)
 
 for ied in ieds:
     df = generate_values_df(sim_data, ied)
